ebdbc8a82170c12f52a6e1cf21a77e1f90557e50
olaposkrobek <aleksandrap@remitly.com>
2026-01-02 19:38:12 +0100

    fix css

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = "wedding";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   trailingSlash: true,
   images: { unoptimized: true },
 };
